f8941c4854f2ed5e2bb0f0667a77cec97c26ba47
Haans Lopez <jesuslopez@ciencias.unam.mx>
2024-05-31 23:33:23 -0600

    adding friendrequest list

--- app02/friendship/urls.py
+++ app02/friendship/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 #from .views import ProfileListView, ProfileDetailView
 
 friendship_patterns = ([
     #path('', ProfileListView.as_view(), name='list'),
     #path('<username>/', ProfileDetailView.as_view(), name='detail'),
+    path('friend_request/',views.friendrequest_list, name='friendrequest_list'),
 ], "friendship")
--- app02/friendship/views.py
+++ app02/friendship/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from registration.models import Profile
+from django.contrib.auth.models import User
+from .models import FriendRequest, Friendship
+
 
 # Create your views here.
+
+def friendrequest_list(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    friend_requests = FriendRequest.objects.filter(to_user=profile)
+    return render(request, 'friendship/friendrequest_list.html', {'friendrequest_list': friend_requests})
+    
